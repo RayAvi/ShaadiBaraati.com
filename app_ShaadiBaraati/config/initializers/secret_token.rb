32c4919c82c7b724ad919978a7957a5e4ea4d71e
RayAbhi2013 <rayabhi2013@gmail.com>
2013-08-15 20:52:43 +0530

    ShaadiBaraati app

---
+++ app_ShaadiBaraati/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppShaadibaraati::Application.config.secret_token = '4c129a546662879ace728a075aaa41f43ce020c08d90db9efbc54764d01354c321646fe5a437c96c09d61000dacb2b49cd62a17ab2c4ef96b99a97a4dc495cca'
